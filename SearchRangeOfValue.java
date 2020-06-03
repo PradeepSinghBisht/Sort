ad8c78f46e04ec627520294293203e8dbafa4a2c
Pradeep Singh Bisht <65757644+PradeepSinghBisht@users.noreply.github.com>
2020-06-03 14:04:39 +0530

    Update SearchRangeOfValue.java

--- SearchRangeOfValue.java
+++ SearchRangeOfValue.java
@@ -1,3 +1,24 @@
+/*
+Problem Statement
+Given an array A of N integers, and a target value K. A is sorted in non-decreasing order. Write an efficient program to find
+the start index and end index of K in A.
+
+Input Format
+First line contains integer N and K.
+Second line contains N space sperated integers, which are members of array.
+
+Output Format
+Print the starting index and ending index of K in array A sepearted by a space. Print -1 if K is not found in A.
+
+Sample Testcase
+Input:
+5 2
+1 2 2 2 3
+
+Output:
+1 3
+*/
+
 package com.company;
 
 import java.util.Arrays;
